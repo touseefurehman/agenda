d7cdc11d5cfedf820ff98d79a6848304b259dc5b
touseefurehman <tousifurehman2084@gmail.com>
2026-01-19 16:50:31 +0500

    allowhost

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import event_schedule
 
 urlpatterns = [
-    path('schedule/', event_schedule, name='event_schedule'),
+    path('', event_schedule, name='event_schedule'),
 ]
