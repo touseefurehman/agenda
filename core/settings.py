4fb4d44d6a59530490c0bf10bd5462f437726d5e
touseefurehman <tousifurehman2084@gmail.com>
2026-01-19 16:47:01 +0500

    allowhost

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m_t-npj$g)dwkb!3^ku%0h2*j^6an!#a&2x$bo+)4l%q#n&il1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["agenda.up.railway.app"]
 
 
 # Application definition
